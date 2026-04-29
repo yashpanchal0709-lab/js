f05abcc2f101cf90c4c1b500ac33427bf864bcd1
yashpanchal0709-lab <yashpanchal0709@gmail.com>
2026-04-29 06:21:44 +0000

    datatypes

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -8,4 +8,27 @@ console.log (3
     +
     3) // code readability should be high
 
-console.log(Yash)
+console.log("Yash")
+
+let name = "Yash"
+let age = 20
+let isLoggedIn = false
+let state
+
+//Primitive Data Types
+// number => 2 to the power of 53 (range)
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value // null is object
+//indefined => Variable value not declared
+// symbol => unique
+
+console.log(typeof "Yash")
+console.log(typeof age)
+
+// Object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //null
+
