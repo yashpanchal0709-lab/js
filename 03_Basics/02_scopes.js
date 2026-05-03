2de24cf7ad447913e1060b3fc6162e1a16242d0b
yashpanchal0709-lab <yashpanchal0709@gmail.com>
2026-05-03 18:28:28 +0000

    scopes arrow and iife

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -3,13 +3,15 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    // console.log("INNER: ", a);
-    
+    // console.log("INNER: ", a); 
 }
 
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+// {} => functions, if-else having is called scope and 
+// not object {}: its for object declaration
 
 function one(){
     const username = "hitesh"
